8ccaee8d6434954434fa7f34c26ce6394b404982
ksj040506 <ksj040506@naver.com>
2025-02-08 18:48:31 +0900

    Title: 데이터 교환, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250208/데이터 교환/exchange-data.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 6, c = 7, tmp1, tmp2;
+        tmp1 = b; tmp2 = c; b = a; c = tmp1; a = tmp2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
